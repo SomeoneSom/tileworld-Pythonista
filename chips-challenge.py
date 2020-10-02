45c33ecf9ad2be90321690a433d57ef4934aa7cc
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2020-10-02 14:40:13 -0700

    Updated to add drawing function

--- chips-challenge.py
+++ chips-challenge.py
@@ -1,6 +1,19 @@
 from console import clear
+from scene import *
 clear()
 pack = "CHIPS.DAT"
+sheet = Texture("img/IMG_1212.PNG")
+x = 1/7
+y = 1/16
+xm = -1
+ym = 16
+off = 1/sheet.size[0]
+textures = {}
+for a in range(112):
+	temp = ym - (a % 16)
+	if a % 16 == 0:
+		xm += 1
+	textures[a] = sheet.subtexture((x*xm, y*temp, x, -y))
 
 #unsigned words are reversed, very weird so this fixes that
 def fix(word_list):
@@ -141,4 +154,61 @@ with open(pack, "rb") as f:
 				break
 			main_byte_count -= offset
 		return [layer1, layer2, traps, movement, cloners, map_title, password, hint_text, time_lim, chip_count]
-	print(get_level())
+		
+	#main code
+	class Game (Scene):
+		
+		def setup(self):
+			self.background_color = "#000000"
+			self.tile_size = (self.size[1] / 9)
+			self.layer1, self.layer2, self.traps, self.movement, self.cloners, self.map_title, self.password, hint_texta, self.time_lim, self.chip_count = get_level()
+			temp = 0
+			self.hint_text = ""
+			for char in hint_texta:
+				if temp >= 15 and char == " ":
+					self.hint_text += "\n"
+					temp = -1
+				self.hint_text += char
+				temp += 1
+			self.curr_time = self.time_lim
+			self.curr_chips = self.chip_count
+			self.display_hint = False
+			self.level = 1
+			self.pos = []
+			for row in self.layer1:
+				if int("6e", 16) in row:
+					self.pos = [self.layer1.index(row), row.index(int("6e", 16))]
+			self.draw()
+			
+		def draw(self):
+			for child in self.children:
+				child.remove_from_parent()
+			to_draw1 = []
+			to_draw2 = []
+			for y in range(4, -5, -1):
+				for x in range(-4, 5):
+					try:
+						to_draw1.append(self.layer1[self.pos[0]-y][self.pos[1]+x])
+					except:
+						to_draw1.append(0)
+					try:
+						to_draw2.append(self.layer2[self.pos[0]-y][self.pos[1]+x])
+					except:
+						to_draw2.append(0)
+			count = 0
+			for y in range(9, 0, -1):
+				for x in range(1, 10):
+					self.add_child(SpriteNode(textures[to_draw2[count]], position = ((self.tile_size * x) - self.tile_size / 2, (self.tile_size * y) - self.tile_size / 2), size = (self.tile_size, self.tile_size)))
+					self.add_child(SpriteNode(textures[to_draw1[count]], position = ((self.tile_size * x) - self.tile_size / 2, (self.tile_size * y) - self.tile_size / 2), size = (self.tile_size, self.tile_size)))
+					count += 1
+			self.add_child(LabelNode("Time: " + str(self.curr_time), position = (self.tile_size * 10.5, self.size[1] - self.tile_size)))
+			self.add_child(LabelNode("Chips: " + str(self.curr_chips), position = (self.tile_size * 10.5, self.size[1] - self.tile_size * 2)))
+			self.add_child(LabelNode("Level " + str(self.level) + ": " + self.map_title, position = (self.tile_size * 10.5, self.size[1] - self.tile_size * 3)))
+			self.add_child(LabelNode("Password: " + self.password, position = (self.tile_size * 10.5, self.size[1] - self.tile_size * 4)))
+			if self.display_hint:
+				self.add_child(LabelNode(self.hint_text, font = ("Helvetica", 20), position = (self.tile_size * 10.5, self.size[1] - self.tile_size * 5)))
+				
+		def update(self):
+			pass
+			
+	run(Game(), LANDSCAPE)
